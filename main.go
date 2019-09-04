035e3e7aa6045e4c4b5cb5c825cbf648f60adfd3
Steve Greenberg <spgreenberg@gmail.com>
2019-09-03 18:32:01 -0600

    display services and training envars

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"strings"
 
 	cfenv "github.com/cloudfoundry-community/go-cfenv"
 )
@@ -13,22 +14,30 @@ import (
 type Index struct {
 	AppName          string
 	AppInstanceIndex int
-	Database         string
-	SpaceName        string
+	AppInstanceGUID  string
+	// Database         string
+	Envars    []string
+	Services  []Service
+	SpaceName string
+}
+
+//Service holds the name and label of a service instance
+type Service struct {
+	Name  string
+	Label string
 }
 
 func main() {
 
-	index := Index{"Unknown", -1, "Unknown", "Unknown"}
+	index := Index{"Unknown", -1, "Unknown", []string{}, []Service{}, "Unknown"}
 
-	template := template.Must(template.ParseFiles("templates/index.html"))
+	template := template.Must(template.ParseFiles("templates/index.html", "templates/kill.html"))
 
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("static"))))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+	if cfenv.IsRunningOnCF() {
 		appEnv, _ := cfenv.Current()
 		if appEnv.Name != "" {
 			index.AppName = appEnv.Name
@@ -36,20 +45,46 @@ func main() {
 		if appEnv.Index > -1 {
 			index.AppInstanceIndex = appEnv.Index
 		}
+		if appEnv.InstanceID != "" {
+			index.AppInstanceGUID = appEnv.InstanceID
+		}
 		if appEnv.SpaceName != "" {
 			index.SpaceName = appEnv.SpaceName
 		}
-
-		for k := range appEnv.Services {
-			index.Database = k
+		for _, svcs := range appEnv.Services {
+			for _, svc := range svcs {
+				index.Services = append(index.Services, Service{svc.Name, svc.Label})
+			}
+		}
+		for _, envar := range os.Environ() {
+			if strings.HasPrefix(envar, "training-") {
+				index.Envars = append(index.Envars, envar)
+			}
 		}
 
+		// config := DBConfig{
+		// 	Hostname: os.Getenv("HOSTNAME"),
+		// 	Name:     os.Getenv("NAME"),
+		// 	Password: os.Getenv("PASSWORD"),
+		// 	Username: os.Getenv("USERNAME"),
+		// }
+
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if err := template.ExecuteTemplate(w, "index.html", index); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
 	http.HandleFunc("/kill", func(w http.ResponseWriter, r *http.Request) {
+		if err := template.ExecuteTemplate(w, "kill.html", index); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+	})
+
+	http.HandleFunc("/killInstance", func(w http.ResponseWriter, r *http.Request) {
 		os.Exit(1)
 	})
 
